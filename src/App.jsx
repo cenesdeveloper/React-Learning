4d860e00896b446e1526a5df4375a0319a8ec00b
cenesdeveloper <cenesdurak@gmail.com>
2025-06-27 23:58:09 -0400

    Added Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-const Card = () => {
+const Card = ({ title }) => {
     return (
-        <div>Card Component</div>
+        <div className="card">{title}</div>
     )
 }
 
 const App = () => {
     return (
-        <div>
-            <h2>Functional Arrow Component</h2>
-
-            <Card />
-            <Card />
-            <Card />
+        <div className="card-container">
+            <Card title="Star Wars" rating={5} isCool={true} actors={[{name:'Actors'}]}/>
+            <Card title="Avatar"/>
+            <Card title="The Lion King"/>
         </div>
     )
 }

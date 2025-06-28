c003fae521c6041178439b96fd3f9e9643718152
cenesdeveloper <cenesdurak@gmail.com>
2025-06-27 23:09:31 -0400

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const Card = () => {
+    return (
+        <div>Card Component</div>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <h2>Functional Arrow Component</h2>
+
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+export default App
